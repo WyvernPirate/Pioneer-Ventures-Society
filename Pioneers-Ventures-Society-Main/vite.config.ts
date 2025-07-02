f7def148aa527b0748a5de536b5eaa41bc295263
Phemelo Moloi <moloieric80@gmail.com>
2025-07-03 00:07:07 +0200

    fix: update path import and formatting in vite.config.ts

--- Pioneers-Ventures-Society-Main/vite.config.ts
+++ Pioneers-Ventures-Society-Main/vite.config.ts
@@ -1,13 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        admin: resolve(__dirname, 'admin.html'),
+      },
     },
   },
 })
