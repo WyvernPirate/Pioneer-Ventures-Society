855c34f565a913b2bc5bd95f7c3fcc714e54e58d
WyvernPirate <moloieric80@gmail.com>
2026-09-01 19:41:10 +0200

    Add envPrefix configuration to vite.config files for environment variable handling

--- Pioneers-Ventures-Society-Main/vite.config.admin.ts
+++ Pioneers-Ventures-Society-Main/vite.config.admin.ts
@@ -4,6 +4,7 @@ import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  envPrefix: ['VITE_', 'NEXT_PUBLIC_'],
   plugins: [react()],
   resolve: {
     alias: {
